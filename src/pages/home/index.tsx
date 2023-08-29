3cc5f6d7a39f5381cb0dbd6a785d3bc0701fe8a8
Ciro Lee <ciroli@CirodeMacBook-Pro.local>
2023-08-29 18:56:17 +0800

    feat: opt text

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 const Home: FC = () => {
   return (
     <div className="flex items-center justify-center h-full">
-      <h2 className="text-3xl font-bold">Advanced Radix Components Demos</h2>
+      <h2 className="text-3xl font-bold">react components based on radix-ui</h2>
     </div>
   );
 };
