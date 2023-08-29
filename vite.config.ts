7af09207160bbd1ab19d30a4871411c33e2ebae0
Ciro Lee <ciroli@CirodeMacBook-Pro.local>
2023-08-29 18:51:57 +0800

    chore: opt ci

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/radix-materials/',
   plugins: [react()],
   resolve: {
     alias: {
