4d4714f2c6f20a5872b41fba1bbaf0374356a9d5
Ciro Lee <ciroli@CirodeMacBook-Pro.local>
2023-09-01 14:32:34 +0800

    style: opt switch style

--- src/packages/components/Switch/index.tsx
+++ src/packages/components/Switch/index.tsx
@@ -54,11 +54,11 @@ const Switch = forwardRef<React.ElementRef<typeof RadixSwitch.Root>, SwitchProps
       onCheckedChange={checkedChangeHandler}
       disabled={props.disabled}
       className={cn(
-        'h-[var(--height)] w-[var(--width)] cursor-default rounded-[var(--height)] box-border bg-gray-200 p-[var(--padding)] transition-colors ease-linear',
+        'h-[var(--height)] w-[var(--width)] cursor-default rounded-[var(--height)] box-border bg-gray-400/40 p-[var(--padding)] transition-colors ease-linear',
         {
           'data-[state=checked]:bg-brand-500': !props.disabled && checked,
-          'data-[disabled]:bg-brand-300 cursor-not-allowed': props.disabled && checked,
-          'data-[disabled]:bg-gray-400/50 cursor-not-allowed': props.disabled && !checked,
+          'data-[disabled]:bg-brand-300/80 cursor-not-allowed': props.disabled && checked,
+          'data-[disabled]:bg-gray-200 cursor-not-allowed': props.disabled && !checked,
         },
         props.className,
       )}
