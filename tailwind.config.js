a2ce24f58c002862a2b02e8a939a52a13682db86
Ciro Lee <ciroli@CirodeMacBook-Pro.local>
2023-12-29 12:22:07 +0800

    chore: change tailwind.plugin to tailwind.theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import themeExtends from './src/packages/plugins/tailwind.plugin';
+import themeExtends from './src/packages/plugins/tailwind.theme';
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
