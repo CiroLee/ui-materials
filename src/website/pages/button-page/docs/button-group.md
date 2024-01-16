1423c8685caef8c1510374d73d0643d5cd43e434
Ciro Lee <ciroli@CirodeMacBook-Pro.local>
2024-01-16 16:40:57 +0800

    docs: update button docs

--- src/website/pages/button-page/docs/button-group.md
+++ src/website/pages/button-page/docs/button-group.md
@@ -3,26 +3,34 @@ import Button, { ButtonGroup } from 'your path';
 
 export default function Demo() {
   return (
-    <div>
+    <div className="flex flex-wrap space-x-3">
       <ButtonGroup className="mb-2">
         <Button>apple</Button>
         <Button>banana</Button>
         <Button>orange</Button>
       </ButtonGroup>
-      <ButtonGroup className="mb-2">
+        <ButtonGroup className="mb-2">
         <Button outline>apple</Button>
         <Button outline>banana</Button>
         <Button outline>orange</Button>
       </ButtonGroup>
+        <ButtonGroup className="mb-2">
+        <Button shape="pill" outline>
+            apple
+        </Button>
+        <Button shape="pill" outline>
+            banana
+        </Button>
+        <Button shape="pill" outline>
+            orange
+        </Button>
+      </ButtonGroup>
       <ButtonGroup className="mb-2">
-        <Button shape="round" outline>
-        apple
+        <Button outline shape="pill">
+            apple
         </Button>
-        <Button shape="round" outline>
-        banana
-        </Button>
-        <Button shape="round" outline>
-        orange
+        <Button outline shape="pill">
+            banana
         </Button>
       </ButtonGroup>
     </div>
--- src/website/pages/button-page/index.tsx
+++ src/website/pages/button-page/index.tsx
@@ -131,27 +131,37 @@ export default function ButtonPage() {
         <Heading as="h4" className="mb-2">
           buttonGroup
         </Heading>
-        <ButtonGroup className="mb-2">
-          <Button>apple</Button>
-          <Button>banana</Button>
-          <Button>orange</Button>
-        </ButtonGroup>
-        <ButtonGroup className="mb-2">
-          <Button outline>apple</Button>
-          <Button outline>banana</Button>
-          <Button outline>orange</Button>
-        </ButtonGroup>
-        <ButtonGroup className="mb-2">
-          <Button shape="pill" outline>
-            apple
-          </Button>
-          <Button shape="pill" outline>
-            banana
-          </Button>
-          <Button shape="pill" outline>
-            orange
-          </Button>
-        </ButtonGroup>
+        <div className="flex flex-wrap space-x-3">
+          <ButtonGroup className="mb-2">
+            <Button>apple</Button>
+            <Button>banana</Button>
+            <Button>orange</Button>
+          </ButtonGroup>
+          <ButtonGroup className="mb-2">
+            <Button outline>apple</Button>
+            <Button outline>banana</Button>
+            <Button outline>orange</Button>
+          </ButtonGroup>
+          <ButtonGroup className="mb-2">
+            <Button shape="pill" outline>
+              apple
+            </Button>
+            <Button shape="pill" outline>
+              banana
+            </Button>
+            <Button shape="pill" outline>
+              orange
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup className="mb-2">
+            <Button outline shape="pill">
+              apple
+            </Button>
+            <Button outline shape="pill">
+              banana
+            </Button>
+          </ButtonGroup>
+        </div>
         <CodeView content={buttonGroupHtml} />
       </ShowBox>
       <Heading as="h2" className="mb-2">
