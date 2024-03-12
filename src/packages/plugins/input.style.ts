b2243105258d54561b19fc47502a8889cf65763d
cirolee <ciromail@126.com>
2024-03-12 15:28:10 +0800

    style: perf input style

--- src/packages/plugins/input.style.ts
+++ src/packages/plugins/input.style.ts
@@ -1,5 +1,5 @@
 export const globalInputStyle = {
-  base: 'flex relative rounded-medium border border-black/10 has-[:focus]:border-brand-500 hover:border-brand-500/60 transition-colors ease-linear',
+  base: 'flex relative rounded-medium border border-black/10 has-[:focus]:border-brand-500 has-[:focus]:shadow-[0_0_0_2px_#e7dafd] hover:border-brand-500/60 transition-all ease-linear',
   input: 'full outline-0 placeholder:text-black/30 bg-transparent',
   tinyBase: 'h-global-ty text-[14px]',
   smallBase: 'h-global-sm text-[14px]',
@@ -10,8 +10,10 @@ export const globalInputStyle = {
   largeInput: 'placeholder:text-[18px] text-[18px]',
   disabledBase: 'bg-black/[0.02] text-black/30 border-black/10 hover:border-black/10 cursor-not-allowed',
   disabledInput: 'cursor-not-allowed',
-  errorBase: 'border-danger-500 hover:border-danger-500/60 has-[:focus]:border-danger-500',
-  warnBase: 'border-warn-500 hover:border-warn-500/60 has-[:focus]:border-warn-500',
+  errorBase:
+    'border-danger-500 hover:border-danger-500/60 has-[:focus]:border-danger-500 has-[:focus]:shadow-[0_0_0_2px_#ffdbd8]',
+  warnBase:
+    'border-warn-500 hover:border-warn-500/60 has-[:focus]:border-warn-500 has-[:focus]:shadow-[0_0_0_2px_#fff2cc]',
   prefix: 'flex flex-center pl-[8px]',
   suffix: 'flex flex-center pr-[8px]',
   clear: `rounded-[12px] flex flex-center duration-200 transition-all scale-0 bg-zinc-300 after:absolute after:rotate-45 after:w-[1px] after:rounded-[1px] select-none 
