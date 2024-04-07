5113b8336956a297194d92a09f589639bb51687b
cirolee <ciromail@126.com>
2024-04-07 18:21:41 +0800

    feat: add description of slider

--- src/website/pages/slider-page/index.tsx
+++ src/website/pages/slider-page/index.tsx
@@ -5,6 +5,7 @@ import ShowBox from '@site/components/ShowBox';
 import Slider from '@ui/Slider';
 import Switch from '@ui/Switch';
 import CodeView from '@site/components/CodeView';
+import Description from '@site/components/Description';
 import { html as baseContent } from './docs/base.md';
 import { html as sizeContent } from './docs/size.md';
 import { html as colorsContent } from './docs/colors.md';
@@ -20,6 +21,7 @@ export default function SliderPage() {
   return (
     <>
       <NavTitle title="Slider" sourceName="Slider" />
+      <Description>A component allows for a range of selection</Description>
       <ShowBox className="mb-4">
         <Heading as="h4" className="mb-2">
           basic
