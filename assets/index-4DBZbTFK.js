3c1ad60771b76e7b10c889ecdf10cad133d1b09b
CiroLee <CiroMail@126.com>
2024-04-10 10:20:22 +0000

    Deploying to gh-pages from @ CiroLee/ui-materials@6c152028f5e75dc748d9502266bc24255f0b5fa9 🚀

---
+++ assets/index-4DBZbTFK.js
@@ -0,0 +1,20 @@
+import{j as s}from"./index-PCb06teX.js";import{N as l,D as t,S as p,g as a,A as e}from"./index-CJk_xHEX.js";import{H as n,C as c}from"./index-DUaNUOXA.js";const r=`<pre class="hljs"><code><span class="hljs-keyword">import</span> <span class="hljs-title class_">Divider</span> <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;your path/Divider&#x27;</span>;
+
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> <span class="hljs-keyword">function</span> <span class="hljs-title function_">Demo</span>(<span class="hljs-params"></span>) {
+  <span class="hljs-keyword">return</span> (
+    <span class="language-xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">className</span>=<span class="hljs-string">&quot;w-fit&quot;</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">p</span>&gt;</span>Divider is used to separate content<span class="hljs-tag">&lt;/<span class="hljs-name">p</span>&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">Divider</span> <span class="hljs-attr">className</span>=<span class="hljs-string">&quot;my-3&quot;</span> /&gt;</span>
+      <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">className</span>=<span class="hljs-string">&quot;h-[24px] relative flex items-center&quot;</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">span</span>&gt;</span>home<span class="hljs-tag">&lt;/<span class="hljs-name">span</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">Divider</span> <span class="hljs-attr">direction</span>=<span class="hljs-string">&quot;vertical&quot;</span> <span class="hljs-attr">className</span>=<span class="hljs-string">&quot;mx-3&quot;</span> /&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">span</span>&gt;</span>about<span class="hljs-tag">&lt;/<span class="hljs-name">span</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">Divider</span> <span class="hljs-attr">direction</span>=<span class="hljs-string">&quot;vertical&quot;</span> <span class="hljs-attr">className</span>=<span class="hljs-string">&quot;mx-3&quot;</span> /&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">span</span>&gt;</span>sponsor<span class="hljs-tag">&lt;/<span class="hljs-name">span</span>&gt;</span>
+        <span class="hljs-tag">&lt;<span class="hljs-name">Divider</span> <span class="hljs-attr">direction</span>=<span class="hljs-string">&quot;vertical&quot;</span> <span class="hljs-attr">className</span>=<span class="hljs-string">&quot;mx-3&quot;</span> /&gt;</span>
+      <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+    <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span></span>
+  )
+}
+</code></pre>
+`,i=[{prop:"direction",desc:"direction of divider component",type:"tag: horizontal | vertical",required:!1,default:"horizontal"},{prop:"className",type:"string",required:!1,default:"-"},{prop:"style",type:"React.CSSProperties",required:!1,default:"-"}];function m(){return s.jsxs(s.Fragment,{children:[s.jsx(l,{title:"Divider",sourceName:"Divider"}),s.jsx(t,{children:"A component used to separate content"}),s.jsxs(p,{className:"mb-4",children:[s.jsx(n,{as:"h4",className:"mb-2",children:"basic"}),s.jsxs("div",{className:"w-fit",children:[s.jsx("p",{children:"Divider is used to separate content"}),s.jsx(a,{className:"my-3"}),s.jsxs("div",{className:"h-[24px] relative flex items-center",children:[s.jsx("span",{children:"home"}),s.jsx(a,{direction:"vertical",className:"mx-3"}),s.jsx("span",{children:"about"}),s.jsx(a,{direction:"vertical",className:"mx-3"}),s.jsx("span",{children:"sponsor"}),s.jsx(a,{direction:"vertical",className:"mx-3"})]})]}),s.jsx(c,{content:r})]}),s.jsx(n,{as:"h2",children:"API"}),s.jsx(e,{rows:i})]})}export{m as default};
